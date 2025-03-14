70d22aaafc2efcb34548c2b2d37e374e73de164e
marlon ureta <fabrisio021@gmail.com>
2025-03-14 12:36:41 -0500

    added useGlobalPipe feature

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 5000;
   await app.listen(port);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      always: true
+    })
+  );
 
   const logger = new Logger('Bootstrap');
   logger.log(`🚀 Servidor corriendo en http://localhost:${port}`);
